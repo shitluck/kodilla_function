2b47964d16454db67bdc6dce56a91217226965a3
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-03-28 15:32:09 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+﻿function getTriangleArea(a,h) {
+    if ((a <= 0) || (h <= 0)) {
+        alert('Nieprawidłowe dane');
+    } else {
+        return a*h/2;
+    }
+}
+console.log( getTriangleArea(10,6));
+
+var getTriangleArea1 = getTriangleArea(4,8);
+console.log( getTriangleArea1 );
+var getTriangleArea2 = getTriangleArea(6,8);
+console.log( getTriangleArea2 );
+var getTriangleArea3 = getTriangleArea(8,12);
+console.log( getTriangleArea3 );
