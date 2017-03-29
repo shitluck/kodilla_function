b227cc62bfcfe67724002bb41b6e205c04b4f0e8
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-03-29 16:11:18 +0200

    function changed in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 ﻿function getTriangleArea(a,h) {
-    if ((a <= 0) || (h <= 0)) {
+    if (isNaN(a,h) == true) {
+        alert('Nieprawidłowe dane');
+    } else if ((a <= 0) || (h <= 0)) {
         alert('Nieprawidłowe dane');
     } else {
         return a*h/2;
     }
 }
-console.log( getTriangleArea(10,6));
+console.log(getTriangleArea(10,6));
 
-var getTriangleArea1 = getTriangleArea(4,8);
+var getTriangleArea1 = getTriangleArea(4,6);
 console.log( getTriangleArea1 );
-var getTriangleArea2 = getTriangleArea(6,8);
+var getTriangleArea2 = getTriangleArea(5,10);
 console.log( getTriangleArea2 );
-var getTriangleArea3 = getTriangleArea(8,12);
+var getTriangleArea3 = getTriangleArea('b','c');
 console.log( getTriangleArea3 );
